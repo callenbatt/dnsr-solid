79c3671d995ad64b87533e00d4f9461229b5f8b0
dnsr <callenbatt@gmail.com>
2022-11-21 22:46:43 -0500

    added icon support

---
+++ src/QueryInput.tsx
@@ -0,0 +1,53 @@
+import { Box, FormControlLabel, IconButton, Input } from "@hope-ui/core";
+import { createSignal, createResource } from "solid-js";
+import { SearchIcon } from "./assets/icons/SearchIcon";
+
+type QueryJSON = {
+  Question?: string[];
+  Answer?: string[];
+};
+const fetchQuery = async (name: string): Promise<QueryJSON> =>
+  (
+    await fetch(`https://cloudflare-dns.com/dns-query?name=${name}`, {
+      headers: { accept: "application/dns-json" },
+    })
+  ).json();
+
+export function QueryInput() {
+  let hostnameInput: HTMLInputElement | undefined;
+
+  const [hostname, setHostname] = createSignal<string>();
+
+  const [query] = createResource(hostname, fetchQuery);
+
+  return (
+    <>
+      <Box
+        display="flex"
+        flexDirection="row"
+        rounded="lg"
+        shadow="lg"
+        w="full"
+        maxW={96}
+        bg="white"
+        _dark={{
+          borderColor: "neutral.800",
+          bg: "neutral.900",
+        }}
+      >
+        <Input placeholder="hostname" ref={hostnameInput} />
+        <IconButton
+          aria-label="Search"
+          variant="solid"
+          onClick={(e) => hostnameInput && setHostname(hostnameInput.value)}
+        >
+          <SearchIcon />
+        </IconButton>
+      </Box>
+
+      <div>
+        <pre>{JSON.stringify(query(), null, 2)}</pre>
+      </div>
+    </>
+  );
+}
--- src/index.tsx
+++ src/index.tsx
@@ -1,7 +1,14 @@
 /* @refresh reload */
-import { render } from 'solid-js/web';
+import { render } from "solid-js/web";
+import { HopeProvider } from "@hope-ui/core";
+import "./index.css";
+import App from "./App";
 
-import './index.css';
-import App from './App';
-
-render(() => <App />, document.getElementById('root') as HTMLElement);
+render(
+  () => (
+    <HopeProvider>
+      <App />
+    </HopeProvider>
+  ),
+  document.getElementById("root") as HTMLElement
+);
